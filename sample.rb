4eb6260684eaa409788b4c1c4b5a17b268ce414e
Tanner Burson <me@tannerburson.com>
2009-06-05 09:42:35 -0500

    Fix whitespace and add a few comments

--- sample.rb
+++ sample.rb
@@ -1,31 +1,33 @@
 # Require sinatra/base instead of sinatra to avoid some of the magic
+# This keeps us from polluting the top level namespace with sinatra's
+# magic methods.
 require 'sinatra/base'
 
 module Sample
   class SampleBase < Sinatra::Base
-	# setup some options we want shared between all 'controllers'
-	set :static, true
-	set :public, File.join(File.dirname(__FILE__),'public')
-	enable :sessions
+    # setup some options we want shared between all 'controllers'
+    set :static, true
+    set :public, File.join(File.dirname(__FILE__),'public')
+    enable :sessions
   end
 
   class Main < SampleBase
-	set :views, File.join(File.dirname(__FILE__),'views','main')
-	# Pretty normal application here
-	get '/' do
-	  erb :index
-	end
+    set :views, File.join(File.dirname(__FILE__),'views','main')
+    # Pretty normal application here
+    get '/' do
+      erb :index
+    end
   end
 
   class Blog < SampleBase
-	set :views, File.join(File.dirname(__FILE__),'views','blog')
-	# We define it this way to not require a hard / at the end of the url
-	get '' do
-	  erb :blog
-	end
+    set :views, File.join(File.dirname(__FILE__),'views','blog')
+    # We define it this way to not require a hard / at the end of the url
+    get '' do
+      erb :blog
+    end
 
-	get '/list' do
-	  erb :blog_list
-	end
+    get '/list' do
+      erb :blog_list
+    end
   end
 end
